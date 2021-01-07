cf989cd28e1a243ccbc45cf85b1875ac1dd7e5d2
Joseph Williams <josephwilliamsjs@gmail.com>
2021-01-07 01:26:29 +0000

    sanitizing user input using parametized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,14 +8,18 @@ const pool = new Pool ({
   database: 'bootcampx'
 });
 
+const cohort = process.argv[2] || 'JUL02';
+const values = [cohort]
+
 pool.query (` 
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM assistance_requests
 JOIN teachers ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher; `
+WHERE cohorts.name = $1
+ORDER BY teacher;`,
+values
 )
 .then(res => {
   for (let obj of res.rows) {
